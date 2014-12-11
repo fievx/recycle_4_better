82454a1b3f87f3935c1b7223c93fdfbc442b7532
fievx <denais.jeremy2@gmail.com>
2014-12-11 16:57:36 +1100

    make Product object parcelable

--- src/com/apps4better/recycle4better/model/Product.java
+++ src/com/apps4better/recycle4better/model/Product.java
@@ -2,9 +2,13 @@ package com.apps4better.recycle4better.model;
 
 import java.util.ArrayList;
 
-public class Product {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Product implements Parcelable {
 	private ArrayList <Element> elementList = new ArrayList <Element>();
 	private String name ;
+	private String description;
 	private int pId;
 	private String photoId;
 	
@@ -15,6 +19,17 @@ public class Product {
 	public Product (ArrayList <Element> list){
 		this.elementList = list;
 	}
+
+	
+	public Product(Parcel source) {
+		// TODO Auto-generated constructor stub
+		source.readList(elementList, null);
+		this.description = source.readString();
+		this.name = source.readString();
+		this.photoId= source.readString();
+		this.pId = source.readInt();
+	}
+
 //////////////////////    GETTERS   ///////////////////
 	public ArrayList<Element> getElementList() {
 		return elementList;
@@ -31,6 +46,11 @@ public class Product {
 public String getPhotoId() {
 		return photoId;
 	}
+
+public String getDescription(){
+	return description;
+}
+
 
 /////////////////////   SETTERS  ////////////////////////
 
@@ -50,6 +70,45 @@ public String getPhotoId() {
 	public void setpId(int pId) {
 		this.pId = pId;
 	}
+
+	
+	public void setDescription (String desc){
+		description = desc;
+	}
+	//////////////// Parcel Methods //////////////
+	
+	
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		// TODO Auto-generated method stub
+		dest.writeList(elementList);
+		dest.writeString(description);
+		dest.writeString(name);
+		dest.writeString(photoId);
+		dest.writeInt(pId);
+	}
+	
+	public static final Parcelable.Creator<Product> CREATOR = new Parcelable.Creator<Product>(){
+
+		@Override
+		public Product createFromParcel(Parcel source) {
+			// TODO Auto-generated method stub
+			return new Product (source);
+		}
+
+		@Override
+		public Product[] newArray(int size) {
+			// TODO Auto-generated method stub
+			return new Product [size];
+		}
+		
+	};
 	
 	
 }
