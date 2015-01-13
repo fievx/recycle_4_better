38479df3e450aab074474181e0081dffe61b417b
fievx <denais.jeremy2@gmail.com>
2015-01-14 08:53:58 +1100

    remove scan constraints

--- src/com/apps4better/recycle4better/view/MainActivity.java
+++ src/com/apps4better/recycle4better/view/MainActivity.java
@@ -141,13 +141,11 @@ public void startScan (){
 public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 	  IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 	  if (scanResult != null) {
-		  if (scanResult.getFormatName().equals("UPC_A")||scanResult.getFormatName().equals("UPC_E") || scanResult.getFormatName().equals("UPC_EAN_EXTENSION")){
 			  long id = Integer.valueOf(scanResult.getContents()).intValue();
 				Intent i = new Intent(context, ProductDetailActivity.class);
 				i.putExtra("product_id", id);
 				i.putExtra("load_info", true);
 				startActivity(i);
-	  }
 	  }	  
 	}
 
